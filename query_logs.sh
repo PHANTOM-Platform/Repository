958ac6d111172affeda750b954cff238db5510dc
Jose Miguel Montanana <jmmontanana@gmail.com>
2018-03-27 15:31:46 +0200

    Delete query_logs.sh

--- query_logs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -XGET 'localhost:9400/repository_db/logs/_search?size=100&sort=date:asc&requestTimeout:Infinity&pretty="true"'
-
-
-curl -XGET 'localhost:9400/repository_db/users/_search?size=100&sort=date:asc&requestTimeout:Infinity&pretty="true"'
-
