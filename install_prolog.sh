1f54f49eaaa15e45f6a2beb86e8893cdd14c94dc
Jose Miguel Montanana <jmmontanana@gmail.com>
2018-05-30 10:30:04 +0200

    Add files via upload

---
+++ install_prolog.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Script for installing the swipl 
+#Tested on ubuntu 17.04
+
+ sudo apt-add-repository ppa:swi-prolog/stable;
+ sudo apt-get update;
+ sudo apt-get install swi-prolog;
+
+
