b34abbfb9210ff092ec6191ffc13ca7a58aef70d
Jose Miguel Montanana <jmmontanana@gmail.com>
2018-05-30 13:52:30 +0200

    Update install_prolog.sh

--- install_prolog.sh
+++ install_prolog.sh
@@ -7,4 +7,11 @@
  sudo apt-get update;
  sudo apt-get install swi-prolog;
 
+#Please try at other linux distributions where the previous procedure doesn't work:
 
+git clone https://github.com/SWI-Prolog/swipl-devel.git
+cd swipl-devel
+make distclean
+git pull
+./prepare
+./build
