56449c14c30ac84cd8acb970eff1eb200a127612
Jose Miguel Montanana <jmmontanana@gmail.com>
2018-06-07 13:49:24 +0200

    Add files via upload

--- test_ws_repo/test_repo_update_db.sh
+++ test_ws_repo/test_repo_update_db.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 #Author: J M Montanana
 # This scripts :
-#  * generates the manager app nodejs
-#  * creates a new DB and register an user
-#  * registers and query an example of a project with few tasks
 #  * updates some fields in the he previous project registered, and adds also some new field
 #
 #  If user is suscribed to a project, then the user will get a copy of the uploading json
-#
 
-#global variables:
-mytoken=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtb250YW5hQGFiYy5jb20iLCJpYXQiOjE1MjQwMzg1NTQsImV4cCI6MTUyNjYzMDU1NH0.1dB8D3E3_L1b6BlRWyTc78H4QvwlJ9mtVt0wNY5V-Uo
-repository_port=8000
-server=localhost
+#. ###################  global variables:
+	repository_port=8000
+	server=localhost
+	source colors.sh;
+# . ################## GET A NEW TOKEN FOR A REGISTERED USER ###################################
+        echo -e "\n${LIGHT_BLUE}";
+        echo "curl -s -H \"Content-Type: text/plain\" -XGET http://${server}:${repository_port}/login?email=\"montana@abc.com\"\&pw=\"new\" --output token.txt\"";
+        read -p $'Press [Enter] key to get an authentication \033[1;37mNEW TOKEN\033[1;34m for the example user'; echo -ne "${NO_COLOUR}";
+        #curl -s -H "Content-Type: text/plain" -XGET http://${server}:${repository_port}/login?email="montana@abc.com"\&pw="new" --output token.txt;
+        curl -s -H "Content-Type: text/plain" -XGET http://${server}:${repository_port}/login?email="hpcjmont@hlrs.de"\&pw="2795572358" --output token.txt;
 
-echo -e "\n\n******************\nREGISTER PROJECT 1" 
+# . ################## SHOW THE TOKEN IN THE SCREEN ###################################
+        echo -e "\n${LIGHT_BLUE}";
+        echo "mytoken=\`cat token.txt\`; echo \${mytoken}";
+        read -p $'Press [Enter] key to \033[1;37mSEE\033[1;34m the received \033[1;37mTOKEN\033[1;34m'; echo -ne "${NO_COLOUR}";
+        mytoken=`cat token.txt;`; echo "token is ${mytoken}";
 
-curl -s -H "Authorization: OAuth ${mytoken}" -H "Content-Type: multipart/form-data" -XPOST -F "UploadFile=@../web/example.c" -F "UploadJSON=@../web/example_pt.json" http://${server}:${repository_port}/upload?DestFileName=main.c\&Path=mypath/
+#. ############################# UPLOAD A FILE ###############################
+	echo -e "\n\n******************\nREGISTER PROJECT 1" 
 
-echo -e "\n\n";
+	curl -s -H "Authorization: OAuth ${mytoken}" -H "Content-Type: multipart/form-data" -XPOST -F "UploadFile=@../web/example.c" -F "UploadJSON=@../web/example_pt.json" http://${server}:${repository_port}/upload?DestFileName=main.c\&Path=mypath/
+
+	echo -e "\n\n";
+
 # NOTICE: When updating the new fields are added to the existing fields
 # NOTICE: BUT, if you update a field which contains one or multiple values, it will replace the value/s of that field, the value/s of a field are not merged on an update!!
 
