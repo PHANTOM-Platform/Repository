c56058d5574bdd14b82732f14b1ce4d125b21252
Jose Miguel Montanana <jmmontanana@gmail.com>
2019-01-21 16:49:28 +0100

    Add files via upload

--- test_ws_repo/test_repo_update_db.sh
+++ test_ws_repo/test_repo_update_db.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 #Author: J M Montanana
 # This scripts :
-#  * updates some fields in the he previous project registered, and adds also some new field
+# * updates some fields in the he previous project registered, and adds also some new field
 #
-#  If user is suscribed to a project, then the user will get a copy of the uploading json
+# If user is suscribed to a project, then the user will get a copy of the uploading json
 
-#. ###################  global variables:
-	repository_port=8000
-	server=localhost
+#. ################### Global variables:
+	repository_port="8000";
+	server="localhost";
 	source colors.sh;
-# . ################## GET A NEW TOKEN FOR A REGISTERED USER ###################################
-        echo -e "\n${LIGHT_BLUE}";
-        echo "curl -s -H \"Content-Type: text/plain\" -XGET http://${server}:${repository_port}/login?email=\"montana@abc.com\"\&pw=\"new\" --output token.txt\"";
-        read -p $'Press [Enter] key to get an authentication \033[1;37mNEW TOKEN\033[1;34m for the example user'; echo -ne "${NO_COLOUR}";
-        #curl -s -H "Content-Type: text/plain" -XGET http://${server}:${repository_port}/login?email="montana@abc.com"\&pw="new" --output token.txt;
-        curl -s -H "Content-Type: text/plain" -XGET http://${server}:${repository_port}/login?email="hpcjmont@hlrs.de"\&pw="2795572358" --output token.txt;
+#. ################## GET A NEW TOKEN FOR A REGISTERED USER ###################################
+	echo -e "\n${LIGHT_BLUE}";
+	echo "curl -s -H \"Content-Type: text/plain\" -XGET http://${server}:${repository_port}/login?email=\"montana@abc.com\"\&pw=\"new\" --output token.txt\"";
+	read -p $'Press [Enter] key to get an authentication \033[1;37mNEW TOKEN\033[1;34m for the example user'; echo -ne "${NO_COLOUR}";
+	curl -s -H "Content-Type: text/plain" -XGET http://${server}:${repository_port}/login?email="montana@abc.com"\&pw="new" --output token.txt;
 
-# . ################## SHOW THE TOKEN IN THE SCREEN ###################################
-        echo -e "\n${LIGHT_BLUE}";
-        echo "mytoken=\`cat token.txt\`; echo \${mytoken}";
-        read -p $'Press [Enter] key to \033[1;37mSEE\033[1;34m the received \033[1;37mTOKEN\033[1;34m'; echo -ne "${NO_COLOUR}";
-        mytoken=`cat token.txt;`; echo "token is ${mytoken}";
+#. ################## SHOW THE TOKEN IN THE SCREEN ###################################
+	echo -e "\n${LIGHT_BLUE}";
+	echo "mytoken=\`cat token.txt\`; echo \${mytoken}";
+	read -p $'Press [Enter] key to \033[1;37mSEE\033[1;34m the received \033[1;37mTOKEN\033[1;34m'; echo -ne "${NO_COLOUR}";
+	mytoken=`cat token.txt;`; echo "token is ${mytoken}";
 
 #. ############################# UPLOAD A FILE ###############################
-	echo -e "\n\n******************\nREGISTER PROJECT 1" 
+	echo -e "\n\n******************\nREGISTER PROJECT 1";
 
-	curl -s -H "Authorization: OAuth ${mytoken}" -H "Content-Type: multipart/form-data" -XPOST -F "UploadFile=@../web/example.c" -F "UploadJSON=@../web/example_pt.json" http://${server}:${repository_port}/upload?DestFileName=main.c\&Path=mypath/
+	curl -s -H "Authorization: OAuth ${mytoken}" -H "Content-Type: multipart/form-data" -XPOST -F "UploadFile=@example.c" -F "UploadJSON=@example_pt.json" http://${server}:${repository_port}/upload?project=demo_hpc\&source=user\&Path=mypath\&DestFileName=main.c
+	echo -e "\n\n";
 
+	echo -e "\n\n******************\nREGISTER PROJECT 2";
+	curl -s -H "Authorization: OAuth ${mytoken}" -H "Content-Type: multipart/form-data" -XPOST -F "UploadFile=@example.c" -F "UploadJSON=@example_manager_task.json" http://${server}:${repository_port}/upload?project=demo_hpc\&source=user\&Path=mypath\&DestFileName=main.c
 	echo -e "\n\n";
 
 # NOTICE: When updating the new fields are added to the existing fields
 # NOTICE: BUT, if you update a field which contains one or multiple values, it will replace the value/s of that field, the value/s of a field are not merged on an update!!
-
--- test_ws_repo/ws_susc_repository.java
+++ test_ws_repo/ws_susc_repository.java
@@ -1,37 +1,37 @@
 package repo_websocket;
 
 import java.net.URI;
-import java.net.URISyntaxException; 
-import org.java_websocket.client.WebSocketClient; 
-import org.java_websocket.handshake.ServerHandshake; 
-import org.java_websocket.exceptions.WebsocketNotConnectedException; 
+import java.net.URISyntaxException;
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+import org.java_websocket.exceptions.WebsocketNotConnectedException;
 
 public class ws_susc_repository extends WebSocketClient {
-	public ws_susc_repository( URI serverURI ) {
+	public ws_susc_repository(URI serverURI) {
 		super( serverURI );
-	} 
+	}
 
 	boolean connection_stablished =false;
 	@Override
-	public void onOpen( ServerHandshake handshakedata ) {
-		System.out.println( "opened connection" );
+	public void onOpen( ServerHandshake handshakedata) {
+		System.out.println("opened connection");
 		connection_stablished=true;
 		// if you plan to refuse connection based on ip or httpfields overload: onWebsocketHandshakeReceivedAsClient
 	}
 
 	@Override
-	public void onMessage( String message ) {  // THIS FUNCTION WILL RUN FOR EACH RECEIVED MESSAGE
-		System.out.println( "received: " + message ); 
+	public void onMessage(String message) { // THIS FUNCTION WILL RUN FOR EACH RECEIVED MESSAGE
+		System.out.println("received: " + message);
 	}
 
 	@Override
-	public void onClose( int code, String reason, boolean remote ) {
+	public void onClose(int code, String reason, boolean remote) {
 		// The codecodes are documented in class org.java_websocket.framing.CloseFrame
-		System.out.println( "Connection closed by " + ( remote ? "remote peer" : "us" ) + " Code: " + code + " Reason: " + reason );
+		System.out.println("Connection closed by " + (remote ? "remote peer" : "us" ) + " Code: " + code + " Reason: " + reason);
 	}
 
 	@Override
-	public void onError( Exception ex ) {
+	public void onError(Exception ex) {
 		ex.printStackTrace();
 		// if the error is fatal then onClose will be called additionally
 	}
@@ -40,11 +40,11 @@ public class ws_susc_repository extends WebSocketClient {
 		String protocol = "ws";
 		String host = "localhost";
 		int port = 8000;
-		String serverlocation = protocol + "://" + host + ":" + port + "/";  
+		String serverlocation = protocol + "://" + host + ":" + port + "/";
 		WebSocketClient ws_client = null;
 		try {
-			ws_client = new ws_susc_repository( new URI( serverlocation ));  // We create a WS connection
-		} catch (URISyntaxException e) {
+			ws_client = new ws_susc_repository( new URI(serverlocation)); // We create a WS connection
+		} catch (URISyntaxException e){
 			e.printStackTrace();
 		}
 		try {
@@ -55,11 +55,10 @@ public class ws_susc_repository extends WebSocketClient {
 		System.out.println("\nSUSCRIBE FOR CONTENTS IN PROJECT demo_hpc:");
 
 		try {
-			ws_client.send("{\"user\":\"alice@abc.com\" , \"project\":\"demo_hpc\"}"); //user is for debugging purposes, it may help to find lost connections
-			ws_client.send("{\"user\":\"alice@abc.com\" , \"source\":\"PT\"}"); //user is for debugging purposes, it may help to find lost connections
-		
+			ws_client.send("{\"user\":\"alice@abc.com\", \"project\":\"demo_hpc\"}"); //user is for debugging purposes, it may help to find lost connections
+			ws_client.send("{\"user\":\"alice@abc.com\", \"source\":\"PT\"}"); //user is for debugging purposes, it may help to find lost connections
 		} catch (WebsocketNotConnectedException ex) {
 			System.out.println("Websocket not connected: " + ex);
 		}
-	} 
+	}
 }
