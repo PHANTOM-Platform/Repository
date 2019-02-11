2eda09fa368264b032b452e5c6b9cbbcc5daf192
Jose Miguel Montanana <jmmontanana@gmail.com>
2019-02-11 15:03:21 +0100

    Add files via upload

---
+++ api_bash_scripts/demo_repo_put_dir.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#Author: J.M.Montañana HLRS 2018
+#Script for create an empty PHANTOM_repository DB.
+
+#Copyright (C) 2018 University of Stuttgart
+#
+# 	Licensed under the Apache License, Version 2.0 (the "License");
+# 	you may not use this file except in compliance with the License.
+# 	You may obtain a copy of the License at
+# 
+# 		http://www.apache.org/licenses/LICENSE-2.0
+# 
+# 	Unless required by applicable law or agreed to in writing, software
+# 	distributed under the License is distributed on an "AS IS" BASIS,
+# 	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# 	See the License for the specific language governing permissions and
+# 	limitations under the License.
+
+################### Global Variables' Definition #############################
+server="localhost"; 
+repository_port="8000";
+app=`basename $0`;
+cd `dirname $0`;
+source colors.sh;
+user="montana@abc.com";#"bob@example.com";
+pw="new";
+################### Testing connectivity with the PHANTOM Repository server: #############
+	source verify_connectivity.sh -s ${server} -port ${repository_port};
+	conectivity=$?;
+	if [ ${conectivity} -eq 1 ]; then
+		echo "[ERROR:] Server \"${server}\" is unreachable on port \"${repository_port}\".";
+		exit 1;
+	fi;
+##### Testing if the PHANTOM Repository server can access to the Elasticsearch Server ####
+	HTTP_STATUS=$(curl -s http://${server}:${repository_port}/verify_es_connection);
+	if [[ ${HTTP_STATUS} != "200" ]]; then
+		echo "PHANTOM Repository Doesn't get Response from the ElasticSearch Server. Aborting.";
+		exit 1;
+	fi;
+# Look which kind of server is listening
+	SERVERNAME=$(curl --silent http://${server}:${repository_port}/servername);
+	if [[ ${SERVERNAME} != "PHANTOM Repository" ]]; then
+		echo " The server found is not a PHANTOM Repository server. Aborting.";
+		echo ${SERVERNAME};
+		exit 1;
+	fi;
+# 6. ################## GET A NEW TOKEN FOR A REGISTERED USER ###################################
+	echo -e "\n${LIGHT_BLUE}";
+	echo "bash get_token.sh -e ${user} -pw ${pw} -s ${server} -port ${repository_port} ;";
+	read -p $'Press [Enter] key to run the script for \033[1;37mOBTAINING\033[1;34m a new token'; echo -ne "${NO_COLOUR}";
+	newtoken=`bash get_token.sh -e ${user} -pw ${pw} -s ${server} -port ${repository_port} ;` ;
+	echo -e "the newtoken is:\n${newtoken}\n";
+# 8. ################## TEST IF A TOKEN IS VALID OR NOT, this is useful when we don't know if the token has expired ####### 
+	echo -e "\n${LIGHT_BLUE}";
+	echo -e "OPTINAL test, not need to perform the verification";
+	echo "bash verify_token.sh -t ${newtoken} -s ${server} -port ${repository_port} ;";
+	read -p $'Press [Enter] key to run the script for \033[1;37mVERIFYING\033[1;34m a token'; echo -ne "${NO_COLOUR}";
+	bash verify_token.sh -t ${newtoken} -s ${server} -port ${repository_port} ;
+# 13. ################## TEST OF UPLOADING A FILE WITH A VALID TOKEN, access must be accepted : 200 ##########
+	echo -e "\n${LIGHT_BLUE}";
+	echo -e "We are currently defining the project and the source in the json file !!!"
+	echo "bash repo_put.sh -t ${newtoken} -sdp \"Application/Component1\" -sjp \"demo.json\" -dp \"Application/Component1\" -s ${server} -port ${repository_port} ";
+	read -p $'Press [Enter] key to run the script for \033[1;37mUPLOADING\033[1;34m a file with metadata'; echo -ne "${NO_COLOUR}";
+	bash repo_put_dir.sh -t ${newtoken} -sdp "Application/Component1" -sjp "demo.json" -pr "demo_put_dir" -sr "development" -dp "Application/Component2" -s ${server} -port ${repository_port};
---
+++ api_bash_scripts/repo_put_dir.sh
@@ -0,0 +1,162 @@
+#!/bin/bash
+#Script for register a new user in the PHANTOM REPOSITORY.
+#Author: J.M.Montañana HLRS 2018
+#  If you find any bug, please notify to hpcjmont@hlrs.de
+
+#Copyright (C) 2018 University of Stuttgart
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+###################   Global Variables' Definition #############################
+server="localhost";
+repository_port="8000";
+app=`basename $0`;
+cd `dirname $0`;
+source colors.sh;
+################ Parse of the input parameters #################################  
+src_file="";
+json_file="";
+dst_file="";
+dst_path="";
+if [ ! $# -eq 0 ]; then
+	nuevo=true;
+	last="";
+	for i do
+		if [ "$nuevo" = true ]; then
+			if [ "$last" = "-s" ] || [ "$last" = "-S" ]; then  #server
+				server=$i;
+				nuevo=false;
+			elif [ "$last" = "-port" ] || [ "$last" = "-PORT" ]; then
+				repository_port=$i;
+				nuevo=false;
+			elif [ "$last" = "-t" ] || [ "$last" = "-T" ]; then #token
+				mytoken=$i;
+				nuevo=false;
+			elif [ "$last" = "-sdp" ] || [ "$last" = "-SDP" ]; then #source path
+				src_dir=$i;
+				nuevo=false;
+			elif [ "$last" = "-sjp" ] || [ "$last" = "-SJP" ]; then #JSON common file
+				json_file=$i;
+				nuevo=false;
+			elif [ "$last" = "-pr" ] || [ "$last" = "-PR" ]; then #project
+				project=$i;
+				nuevo=false;
+			elif [ "$last" = "-sr" ] || [ "$last" = "-SR" ]; then #first folder 
+				source=$i;
+				nuevo=false;
+			elif [ "$last" = "-dp" ] || [ "$last" = "-DP" ]; then #other level folders
+				dst_path=$i;
+				nuevo=false;
+			elif [ "$i" = "-h" ] || [ "$i" = "-H" ]; then
+				echo -e "${yellow}Script for UPLOADING a DIRECTORY plus METADATA${reset}";
+				echo -e "${yellow}Syntax ${app}:${reset}";
+				echo -e "${yellow}   Required fields:${reset}";
+				echo -e "${yellow}      authentication token  [-t f7vglñerghpq3ghwoghw] ${reset}";
+				echo -e "${yellow}      source_dir_path      [-sdp 1234] ${reset}"; # ../web/example.h      src_file
+				echo -e "${yellow}      source_json_path     [-sjp 1234] ${reset}"; # ../web/exampleh.json  json_file
+
+				echo -e "${yellow}      project              [-pr 1234] ${reset}"; # project
+				echo -e "${yellow}      source               [-sr 1234] ${reset}"; # source
+				echo -e "${yellow}      destination_path     [-dp 1234] ${reset}"; # main.h dst_file
+
+				echo -e "${yellow}   Optional fields:${reset}";
+				echo -e "${yellow}      Server [-s phantom.com] ${reset}";
+				echo -e "${yellow}      Port [-port 8000] ${reset}"	;
+				echo -e "${yellow}   Help [--help] to get this help.${reset}" ;
+				echo -e "\n${yellow} Example of use:\n     ${app}  -t ${newtoken}  -sdp \"../web/\" -sjp \"demo.json\" -pr \"phantom_tools_demo\" -sr \"development\" -df \"mypath\" ${reset}\n";
+				echo -e "\n NOTICE: The \"path\" if defined in the JSON MUST will be REPLACED by the MANDATORY input parameters \"destination_path\"";
+				exit 0;
+			elif [ "$last" != "" ]; then
+				echo "error de sintaxis" $last ".";
+				exit 1;
+			else
+				last=$i;
+			fi;
+		else
+			nuevo=true;
+			last=$i;
+		fi;
+	done;
+fi;
+
+if [ -z "${src_dir}" ]; then
+    echo -e "Missing parameter Input File: sdp\n";
+    exit 1;
+fi;
+if [ -z "${json_file}" ]; then
+    echo -e "Missing parameter Input JSON: sjp\n";
+    exit 1;
+fi;
+if [ -z "${project}" ]; then
+	echo -e "Missing parameter Project: pr\n";
+    exit 1;
+fi;
+if [ -z "${source}" ]; then
+	echo -e "Missing parameter Source: sr\n";
+    exit 1;
+fi;
+if [ -z "${dst_path}" ]; then
+	echo -e "Missing parameter Destination Path: dp\n";
+    exit 1;
+fi;
+
+################### Testing connectivity with the PHANTOM Repository server: #############
+	source verify_connectivity.sh -s ${server} -port ${repository_port};
+	conectivity=$?;
+	if [ ${conectivity} -eq 1 ]; then
+		echo "[ERROR:] Server \"${server}\" is unreachable on port \"${repository_port}\".";
+		exit 1;
+	fi;
+##### Testing if the PHANTOM Repository server can access to the Elasticsearch Server ####
+	HTTP_STATUS=$(curl -s http://${server}:${repository_port}/verify_es_connection);
+	if [[ ${HTTP_STATUS} != "200" ]]; then
+		echo "PHANTOM Repository Doesn't get Response from the ElasticSearch Server. Aborting.";
+		exit 1;
+	fi;
+# Look which kind of server is listening
+	SERVERNAME=$(curl --silent http://${server}:${repository_port}/servername);
+	if [[ ${SERVERNAME} != "PHANTOM Repository" ]]; then
+		echo " The server found is not a PHANTOM Repository server. Aborting.";
+		echo ${SERVERNAME};
+		exit 1;
+	fi;
+######## UPLOAD file and metadata ###################################################
+	if [ ! -e ${src_dir} ]; then
+		echo "error, src-file \"${src_file}\" not found!!";
+		exit 1;
+	fi;
+	if [ ! -e ${json_file} ]; then
+		echo "error, json-file \"${json_file}\" not found!!";
+		exit 1;
+	fi;
+	echo "";
+	
+#$1 is the recursing path 
+#$2 is the prefix to cut
+recurse() {
+ for i in "$1"/*;do
+    if [ -d "$i" ];then
+#         echo "dir: $i"
+        recurse "$i" $2
+    elif [ -f "$i" ]; then
+        echo -e "\nProcessing file: $i"
+        file_name=${i#$1/}
+        subpath=${i#$2/}
+        subpath=${subpath%/$file_name}
+        echo -e " repo_put.sh source="development" source_file=$i dest_path=${dst_path}/${subpath} filename=${file_name};\n"
+		curl -H "Authorization: OAuth ${mytoken}" -H "Content-Type: multipart/form-data" -XPOST -F "UploadFile=@${i}" -F "UploadJSON=@${json_file}" http://${server}:${repository_port}/upload?project=${project}\&source=${source}\&Path=${dst_path}/${subpath}\&DestFileName=${file_name}
+	fi
+ done
+}
+
+recurse ${src_dir} ${src_dir}
